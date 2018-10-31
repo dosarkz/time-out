7d48eed71086c72869824ad148ee449130fe8cf2
dosarkz <ashenov.e@gmail.com>
2018-10-31 16:42:17 +0600

    Добавьте файлы проекта.

---
+++ Sheduller/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Sheduller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.everyMinute = new System.Windows.Forms.RadioButton();
+            this.everyHour = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.everyMinute);
+            this.groupBox1.Controls.Add(this.everyHour);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 64);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Timeout";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // everyMinute
+            // 
+            this.everyMinute.AutoSize = true;
+            this.everyMinute.Location = new System.Drawing.Point(108, 26);
+            this.everyMinute.Name = "everyMinute";
+            this.everyMinute.Size = new System.Drawing.Size(86, 17);
+            this.everyMinute.TabIndex = 1;
+            this.everyMinute.TabStop = true;
+            this.everyMinute.Text = "Every minute";
+            this.everyMinute.UseVisualStyleBackColor = true;
+            // 
+            // everyHour
+            // 
+            this.everyHour.AutoSize = true;
+            this.everyHour.Location = new System.Drawing.Point(17, 26);
+            this.everyHour.Name = "everyHour";
+            this.everyHour.Size = new System.Drawing.Size(76, 17);
+            this.everyHour.TabIndex = 0;
+            this.everyHour.TabStop = true;
+            this.everyHour.Text = "Every hour";
+            this.everyHour.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 92);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(413, 199);
+            this.listBox1.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(441, 304);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sheduller";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton everyMinute;
+        private System.Windows.Forms.RadioButton everyHour;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Sheduller/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Sheduller
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32")]
+        public static extern void LockWorkStation();
+
+        bool on = false;
+        string value = "";
+        Timer timer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Text = "On";
+            timer.Enabled = false;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (everyHour.Checked)
+            {
+                value = "hour";
+            }
+            else if (everyMinute.Checked)
+            {
+                value = "minute";
+            }
+         
+
+            if (on)
+            {
+                button1.Text = "On";
+                on = false;
+
+                timer.Enabled = false;
+            }
+            else
+            {
+                button1.Text = "Off";
+                on = true;
+
+                timer.Interval = 1000;
+                timer.Enabled = true;
+                timer.Tick += new System.EventHandler(OnTimerEvent);
+            }
+        }
+
+        private void OnTimerEvent(object sender, EventArgs e)
+        {
+            if (value == "hour")
+            {
+                if (DateTime.Now.Minute == 00 && DateTime.Now.Second == 00)
+                {
+                    LockWorkStation();
+                }
+            }
+
+            else if (value == "minute")
+            {
+                if (DateTime.Now.Second == 00)
+                {
+                    LockWorkStation();
+                }
+            }
+
+
+            listBox1.Items.Add(DateTime.Now.ToLongTimeString() + "," + DateTime.Now.ToLongDateString());
+        }
+       
+
+        private void sendMessage(string value)
+        {
+            string message = "Please stand up and go to the walk for 10 minites!!!";
+            string caption = "Stand up time every!" + value;
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result;
+
+            // Displays the MessageBox.
+
+            result = MessageBox.Show(message, caption, buttons);
+
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+
+                // Closes the parent form.
+
+                this.Close();
+
+            }
+        }
+    }
+}
